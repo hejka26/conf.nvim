c24145075e4b35eacb4aa97e19391680f5f06cfe
Kasia Jankowska <hejka26@gmail.com>
2023-12-23 17:09:17 +0100

    Move telescope plugin into it's own file

--- init.lua
+++ init.lua
@@ -74,27 +74,7 @@ require('lazy').setup({
 
   require 'config.plugins.visual',
 
-  -- Fuzzy Finder (files, lsp, etc)
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-    },
-  },
-
+  require 'config.plugins.telescope',
 -- NOTE: Following line can supposedly auto import all lua files from set folder, need to figure it out
 -- { import = 'custom.plugins' },
 }, {})
