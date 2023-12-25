ac610314fa5959e335087b4f15155da7a5c28094
Kasia Jankowska <hejka26@gmail.com>
2023-12-25 23:38:19 +0100

    Install formatter and run it on my config

--- lua/config/code-basics.lua
+++ lua/config/code-basics.lua
@@ -39,8 +39,8 @@ local on_attach = function(_, bufnr)
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+    require('conform').format()
+  end, { desc = 'Format current buffer with Conform' })
 end
 
 -- document existing key chains
@@ -79,10 +79,10 @@ local servers = {
     settings = {
       ['rust-analyzer'] = {
         checkOnSave = {
-          command = 'clippy'
-        }
-      }
-    }
+          command = 'clippy',
+        },
+      },
+    },
   },
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
--- lua/config/plugins/code-basics.lua
+++ lua/config/plugins/code-basics.lua
@@ -46,4 +46,22 @@ return {
       },
       build = ':TSUpdate',
   },
+
+  {
+    -- Formatter support
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+          lua = { 'stylua' },
+          rust = { 'rustfmt' },
+          python = { 'autopep8' },
+          go = { 'gofmt' },
+        },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  }
 }
