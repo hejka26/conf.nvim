82d6b6c35229b53ff1651a5c2bda20c918220cb5
Kasia Jankowska <hejka26@gmail.com>
2024-01-02 03:31:45 +0100

    Move debug config into seperate file

--- init.lua
+++ init.lua
@@ -120,5 +120,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require 'config.telescope'
 
 require 'config.code-basics'
+
+require 'config.plugins.debug'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
