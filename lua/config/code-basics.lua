f835e479b24de3228fb471a1e479c3da78f40d49
Kasia Jankowska <hejka26@gmail.com>
2024-10-26 15:26:39 +0200

    Add automatic loading of hyprls

--- lua/config/code-basics.lua
+++ lua/config/code-basics.lua
@@ -98,6 +98,20 @@ local servers = {
   hyprls = {},
 }
 
+-- Make nvim load hyprls when configing hypr apps.
+-- Hyprlang LSP
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+		pattern = {"*.hl", "hypr*.conf"},
+		callback = function(event)
+				print(string.format("starting hyprls for %s", vim.inspect(event)))
+				vim.lsp.start {
+						name = "hyprlang",
+						cmd = {"hyprls"},
+						root_dir = vim.fn.getcwd(),
+				}
+		end
+})
+
 -- Setup neovim lua configuration
 require('neodev').setup()
 
