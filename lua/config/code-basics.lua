2222e46c8163e3a8398542d7ef39f578419bf6bc
Kasia Jankowska <hejka26@gmail.com>
2025-10-10 11:39:01 +0200

    I used wrong name for clangformat, eto, bleh.

--- lua/config/code-basics.lua
+++ lua/config/code-basics.lua
@@ -274,7 +274,7 @@ cmp.setup {
 require('conform').setup {
   formatters_by_ft = {
     lua = { 'stylua' },
-    cpp = { 'clangformat' },
+    cpp = { 'clang-format' },
     rust = { 'rustfmt', lsp_format = 'fallback' },
     javascript = { 'biome' },
     typescript = { 'biome' },
