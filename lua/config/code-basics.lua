5b28e54d347752205d964d02d5d2957dd3fcb41c
Kasia Jankowska <hejka26@gmail.com>
2025-10-16 11:16:53 +0200

    Replace on_attach

--- lua/config/code-basics.lua
+++ lua/config/code-basics.lua
@@ -1,42 +1,79 @@
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+  callback = function(event)
+    -- NOTE: Remember that Lua is a real programming language, and as such it is possible
+    -- to define small helper and utility functions so you don't have to repeat yourself.
+    --
+    -- In this case, we create a function that lets us more easily define mappings specific
+    -- for LSP related items. It sets the mode, buffer and description for us each time.
+    local map = function(keys, func, desc, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+    map('<leader>cr', vim.lsp.buf.rename, '[c]ode [r]ename')
+    map('<leader>ca', vim.lsp.buf.code_action, '[c]ode [a]ction')
+
+    map('gd', require('telescope.builtin').lsp_definitions, '[g]oto [d]efinition')
+    map('gr', require('telescope.builtin').lsp_references, '[g]oto [r]eferences')
+    map('gI', require('telescope.builtin').lsp_implementations, '[g]oto [I]mplementation')
+    map('gy', require('telescope.builtin').lsp_type_definitions, '[g]oto t[y]pe Definition')
+    map('<leader>fs', require('telescope.builtin').lsp_document_symbols, '[f]ind document [s]ymbols')
+    map('<leader>fS', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[f]ind workspace [S]ymbols')
+
+    -- See `:help K` for why this keymap
+    map('K', vim.lsp.buf.hover, 'Hover Documentation')
+    map('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+    -- Lesser used LSP functionality
+    map('gD', vim.lsp.buf.declaration, '[g]oto [D]eclaration')
+    map('<leader>wa', vim.lsp.buf.add_workspace_folder, '[w]orkspace [a]dd Folder')
+    map('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[w]orkspace [r]emove Folder')
+    map('<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[w]orkspace [l]ist Folders')
+
+    -- The following two autocommands are used to highlight references of the
+    -- word under your cursor when your cursor rests there for a little while.
+    --    See `:help CursorHold` for information about when this is executed
+    --
+    -- When you move your cursor, the highlights will be cleared (the second autocommand).
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client then
+      local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+        end,
+      })
     end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>cr', vim.lsp.buf.rename, '[c]ode [r]ename')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[c]ode [a]ction')
-
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[g]oto [d]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[g]oto [r]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[g]oto [I]mplementation')
-  nmap('gy', require('telescope.builtin').lsp_type_definitions, '[g]oto t[y]pe Definition')
-  nmap('<leader>fs', require('telescope.builtin').lsp_document_symbols, '[f]ind document [s]ymbols')
-  nmap('<leader>fS', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[f]ind workspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[g]oto [d]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[w]orkspace [a]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[w]orkspace [r]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[w]orkspace [l]ist Folders')
-end
+    -- The following code creates a keymap to toggle inlay hints in your
+    -- code, if the language server you are using supports them
+    --
+    -- This may be unwanted, since they displace some of your code
+    if client then
+      map('<leader>th', function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+      end, '[t]oggle Inlay [h]ints')
+    end
+  end,
+})
 
 -- document existing key chains
 require('which-key').add {
