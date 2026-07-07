86e2f0211f4d269a0df33ba01537e6cb47ef5069
unknown <hejka26@gmail.com>
2026-07-07 13:39:14 +0200

    feat: add leader O keymap to open Oil in current buffer directory

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,3 +14,6 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- File Explorer (Oil) keymap
+vim.keymap.set('n', '<leader>O', '<cmd>Oil<cr>', { desc = '[O]pen parent directory in Oil' })
